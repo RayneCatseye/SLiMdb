26180bf982b2e47df59c89c611ade2db83643a4a
Emily Saunders Walmsley <EmSaundWalm@gmail.com>
2015-11-26 19:05:14 +1100

    EVERYTHING IS BROKEN

---
+++ occ_data.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+
+import psycopg2
+import re
+import csv
+
+try:
+	conn = psycopg2.connect(database="slimdbtest", user="rayne")
+except:
+	print "Unable to connect to database"
+
+cur = conn.cursor()
+
+f = open('MainData/humsf09_slimfinder.occ.csv', 'r')
+
+reader = csv.reader(f, delimiter=',', quotechar='"')
+for line in reader:
+	if line[0] != "Hub":
+		print len(line)
+		print line
+		cur.execute("""INSERT INTO occresults (hub, dataset, rank, pattern, gene, sig, seq, start_pos, end_pos, prot_len, match, variant, mismatch, description, cons, homnum, globid, locid, iup, sa, pepseq, pepdesign) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)""", (line[0], line[1], line[2], line[3], line[4], line[5], line[6], line[7], line[8], line[9], line[10], line[11], line[12], line[13], line[14], line[15], line[16], line[17], line[18], line[19], line[20], line[21]))
+
+conn.commit()
+
+cur.close()
+conn.close()
+
