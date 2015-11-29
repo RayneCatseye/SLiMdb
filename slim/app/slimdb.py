77d0f1d1bb50cc9c9872505d379a03d32d150ff7
Sharif Olorin <sio@tesser.org>
2015-11-29 06:06:15 +0000

    Grab form data

--- slim/app/slimdb.py
+++ slim/app/slimdb.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 
 flask_cfg = {
     "DATABASE" : "dbname=slimdb user=slimdb password=slimdb"
@@ -13,19 +13,25 @@ def index():
     return render_template("index.html")
 
 # Search and display data from the `gene` table.
-@ui.route("/gene")
+@ui.route("/gene", methods=["GET", "POST"])
 @ui.route("/gene/<gene_name>")
 def by_gene(gene_name=None):
+    if request.method == "POST":
+        gene_name = request.form.get("gene_name", None)
+        print(gene_name)
+        print(request.form)
+        if gene_name:
+            return redirect(url_for("by_gene", gene_name=gene_name))
     return render_template("gene.html", gene_name=gene_name, genes=[])
 
 # Search and display data from the `pfam_domain` table.
-@ui.route("/domain")
+@ui.route("/domain", methods=["GET", "POST"])
 @ui.route("/domain/<domain_name>")
 def by_domain(domain_name=None):
     return render_template("domain.html", domain_name=domain_name, domains=[])
 
 # Search and display data from the `occ_results` table.
-@ui.route("/pattern")
+@ui.route("/pattern", methods=["GET", "POST"])
 @ui.route("/pattern/<pattern_name>")
 def by_pattern(pattern_name=None):
     return render_template("pattern.html", pattern_name=pattern_name, patterns=[])
