513de33b8c1ed59b5d1c1613cb213f0eccd109ed
Sharif Olorin <sio@tesser.org>
2015-11-29 05:31:56 +0000

    Add navbar and page templates

--- slim/app/slimdb.py
+++ slim/app/slimdb.py
@@ -12,4 +12,24 @@ ui.debug = True
 def index():
     return render_template("index.html")
 
+# Search and display data from the `gene` table.
+@ui.route("/gene")
+@ui.route("/gene/<gene_name>")
+def by_gene(gene_name=None):
+    return render_template("gene.html", gene_name=gene_name, genes=[])
+
+# Search and display data from the `pfam_domain` table.
+@ui.route("/domain")
+@ui.route("/domain/<domain_name>")
+def by_domain(domain_name=None):
+    return render_template("domain.html", domain_name=domain_name, domains=[])
+
+# Search and display data from the `occ_results` table.
+@ui.route("/pattern")
+@ui.route("/pattern/<pattern_name>")
+def by_pattern(pattern_name=None):
+    return render_template("pattern.html", pattern_name=pattern_name, patterns=[])
+
+
+
 
